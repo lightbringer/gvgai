9a68a04911f10f63e96fd53e58bb2fb68349fb9d
diegopliebana <akilascartas@gmail.com>
2015-03-27 14:11:39 +0000

    more initial tests

--- src/LearningTest.java
+++ src/LearningTest.java
@@ -24,12 +24,12 @@ public class LearningTest
         String gamesPath = "examples/gridphysics/";
 
         //CIG 2014 Training Set Games
-        //String games[] = new String[]{"aliens", "boulderdash", "butterflies", "chase", "frogs",
-        //        "missilecommand", "portals", "sokoban", "survivezombies", "zelda"};
+        String games[] = new String[]{"aliens", "boulderdash", "butterflies", "chase", "frogs",
+                "missilecommand", "portals", "sokoban", "survivezombies", "zelda"};
 
         //CIG 2014 Validation Set Games
-        String games[] = new String[]{"camelRace", "digdug", "firestorms", "infection", "firecaster",
-              "overload", "pacman", "seaquest", "whackamole", "eggomania"};
+        //String games[] = new String[]{"camelRace", "digdug", "firestorms", "infection", "firecaster",
+        //      "overload", "pacman", "seaquest", "whackamole", "eggomania"};
 
         //CIG 2015 New Training Set Games
         //String games[] = new String[]{"bait", "boloadventures", "brainman", "chipschallenge",  "modality",
@@ -42,7 +42,7 @@ public class LearningTest
         int seed = new Random().nextInt();
 
         //Game and level to play
-        int gameIdx = 6;
+        int gameIdx = 2;
         int levelIdx = 0; //level names from 0 to 4 (game_lvlN.txt).
         String game = gamesPath + games[gameIdx] + ".txt";
         String level1 = gamesPath + games[gameIdx] + "_lvl" + levelIdx +".txt";
