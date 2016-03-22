7d0a014a254d8c218225572ac50127101e89a2f0
Spyros Samothrakis <ssamot@essex.ac.uk>
2016-03-22 14:38:39 +0000

    Fixes on Learning track

---
+++ clients/java_learning_client/src/JavaClient.java
@@ -0,0 +1,13 @@
+/**
+ * Example of a Java Client.
+ * User: Diego
+ * Date: 16/03/15
+ */
+public class JavaClient
+{
+    public static void main(String[] args)
+    {
+        ClientComm ccomm = new ClientComm();
+        ccomm.start();
+    }
+}
--- src/core/player/LearningPlayer.java
+++ src/core/player/LearningPlayer.java
@@ -9,6 +9,11 @@ import tools.ElapsedCpuTimer;
 import java.awt.*;
 import java.io.*;
 import java.util.ArrayList;
+import java.util.logging.FileHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
+
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,7 +23,9 @@ import java.util.ArrayList;
  * This is a Java port from Tom Schaul's VGDL - https://github.com/schaul/py-vgdl
  * http://stackoverflow.com/questions/4112470/java-how-to-both-read-and-write-to-from-process-thru-pipe-stdin-stdout
  */
-public class LearningPlayer extends AbstractPlayer{
+public class LearningPlayer extends AbstractPlayer {
+
+    private static final Logger logger = Logger.getLogger(LearningPlayer.class.getName());
 
     /**
      * Last action executed by this agent.
@@ -45,79 +52,92 @@ public class LearningPlayer extends AbstractPlayer{
      */
     private Process client;
 
-    /**
-     * Print debug info
-     */
-    private boolean verbose;
 
     /**
      * Public constructor of the player.
      * @param client process that runs the agent.
      */
-    public LearningPlayer(Process client)
-    {
-        isLearner = true;
-        verbose = false;
+
+
+    static {
+
+        FileHandler fh;
 
         try {
 
-            this.client = client;
-            initBuffers();
+            // This block configure the logger with handler and formatter
+            fh = new FileHandler("./debug.txt");
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();
+            fh.setFormatter(formatter);
 
-        }catch(Exception e)
-        {
-            System.out.println("Exception creating the client process: " + e);
+            // the following statement is used to log any messages
+
+
+        } catch (SecurityException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+
+    }
+
+    public LearningPlayer(Process client) {
+        isLearner = true;
+
+
+        this.client = client;
+        initBuffers();
+
+
     }
 
     /**
      * Creates the buffers for pipe communication.
      */
-    private void initBuffers()
-    {
-        try {
-            input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-            output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+    private void initBuffers() {
 
-        }catch(Exception e)
-        {
-            System.out.println("Exception creating the client process: " + e);
-            e.printStackTrace();
-        }
+        input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        output = new BufferedWriter(new OutputStreamWriter(client.getOutputStream()));
+
+
     }
 
     /**
      * Picks an action. This function is called at the beginning of the game for
      * initialization.
-     * @param stateView View of the current state.
+     *
+     * @param stateView    View of the current state.
      * @param elapsedTimer Timer when the initialization is due to finish.
-     * @param isTraining true if this game is played as training.
+     * @param isTraining   true if this game is played as training.
      */
-    public void init(StateView stateView, ElapsedCpuTimer elapsedTimer, boolean isTraining)
-    {
+    public void init(StateView stateView, ElapsedCpuTimer elapsedTimer, boolean isTraining) {
         isLearner = true;
-        verbose = false;
+
 
         stateView.assignPlayer(this);
 
-        //Sending messages.
-        commSend ("INIT " + isTraining);
+        try {
 
-        commSend(stateView.getGameInfo(false));
-        commSend(stateView.getActionsInfo());
-        commSend(stateView.getAvatarInfo());
+            //Sending messages.
+            commSend("INIT " + isTraining);
 
-        int nSprites = VGDLRegistry.GetInstance().numSpriteTypes();
-        for(int i = 0; i < nSprites; ++i)
-        {
-            commSend(stateView.getBitGrid(i));
+            commSend(stateView.getGameInfo(false));
+            commSend(stateView.getActionsInfo());
+            commSend(stateView.getAvatarInfo());
+
+            int nSprites = VGDLRegistry.GetInstance().numSpriteTypes();
+            for (int i = 0; i < nSprites; ++i) {
+                commSend(stateView.getBitGrid(i));
+            }
+
+            commSend("INIT-END " + elapsedTimer.remainingTimeMillis());
+
+            String response = commRecv(elapsedTimer, "INIT");
+            logger.fine("Received: " + response);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-
-        commSend ("INIT-END " + elapsedTimer.remainingTimeMillis());
-
-        String response = commRecv(elapsedTimer, "INIT");
-        if(verbose) System.out.println("Received: " + response);
     }
 
 
@@ -126,114 +146,104 @@ public class LearningPlayer extends AbstractPlayer{
      * action from the player. The action returned must be contained in the
      * actions accessible from stateObs.getAvailableActions(), or action NIL
      * will be applied.
-     * @param stateObs Observation of the current state.
+     *
+     * @param stateObs     Observation of the current state.
      * @param elapsedTimer Timer when the action returned is due.
      * @return An action for the current state
      */
-    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer)
-    {
+    public Types.ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
         initBuffers();
-        verbose = false;
+
         //Pipes here.
-        StateView sv = (StateView)stateObs;
+        StateView sv = (StateView) stateObs;
 
         //Sending messages.
-        commSend ("ACT");
+        try {
+            commSend("ACT");
 
-        commSend(sv.getGameInfo(true));
-        // commSend(sv.getActionsInfo()); //NOT NEEDED (they don't change).
-        commSend(sv.getAvatarInfo());
 
-        int nSprites = VGDLRegistry.GetInstance().numSpriteTypes();
-        for(int i = 0; i < nSprites; ++i)
-        {
-            commSend(sv.getBitGrid(i));
+            commSend(sv.getGameInfo(true));
+            // commSend(sv.getActionsInfo()); //NOT NEEDED (they don't change).
+            commSend(sv.getAvatarInfo());
+
+            int nSprites = VGDLRegistry.GetInstance().numSpriteTypes();
+            for (int i = 0; i < nSprites; ++i) {
+                commSend(sv.getBitGrid(i));
+            }
+
+            commSend("ACT-END " + elapsedTimer.remainingTimeMillis());
+
+            String response = commRecv(elapsedTimer, "ACT");
+            logger.fine("Received ACTION: " + response + "; ACT Response time: "
+                    + elapsedTimer.elapsedMillis() + " ms.");
+            Types.ACTIONS action = Types.ACTIONS.fromString(response);
+            return action;
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
-        commSend ("ACT-END " + elapsedTimer.remainingTimeMillis());
-
-        String response = commRecv(elapsedTimer, "ACT");
-        if(verbose)  System.out.println("Received ACTION: " + response + "; ACT Response time: "
-                                        + elapsedTimer.elapsedMillis() + " ms.");
-
-        Types.ACTIONS action = Types.ACTIONS.fromString(response);
-        return action;
     }
 
 
-    public void finishGame(StateObservation stateObs, ElapsedCpuTimer elapsedTimer)
-    {
+    public void finishGame(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) throws IOException {
         initBuffers();
-        verbose = false;
+
         //Pipes here.
-        StateView sv = (StateView)stateObs;
+        StateView sv = (StateView) stateObs;
 
         //Sending messages.
-        commSend ("ENDGAME");
+        commSend("ENDGAME");
 
         commSend(sv.getGameInfo(true));
         // commSend(sv.getActionsInfo()); //NOT NEEDED (they don't change).
         commSend(sv.getAvatarInfo());
 
         int nSprites = VGDLRegistry.GetInstance().numSpriteTypes();
-        for(int i = 0; i < nSprites; ++i)
-        {
+        for (int i = 0; i < nSprites; ++i) {
             commSend(sv.getBitGrid(i));
         }
 
-        commSend ("ENDGAME-END " + elapsedTimer.remainingTimeMillis());
+        commSend("ENDGAME-END " + elapsedTimer.remainingTimeMillis());
 
         String response = commRecv(elapsedTimer, "ENDGAME");
-        if(verbose) System.out.println("Received: " + response);
+        logger.fine("Received: " + response);
     }
 
     /**
      * Sends a message through the pipe.
+     *
      * @param msg message to send.
      */
-    public void commSend(String msg) {
-        String ret;
+    public void commSend(String msg) throws IOException {
 
-        if(verbose)
-            System.out.println(msg);
+        output.write(msg + lineSep);
+        output.flush();
 
-        try {
-            output.write( msg + lineSep );
-            output.flush();
-        }
-        catch (Exception err) {
-            System.out.println("Exception sending message: " + err);
-            err.printStackTrace();
-        }
     }
 
     /**
      * Waits for a response during T milliseconds.
+     *
      * @param elapsedTimer Timer when the initialization is due to finish.
-     * @param idStr String identifier of the phase the communication is in.
+     * @param idStr        String identifier of the phase the communication is in.
      * @return the response got from the client, or null if no response was received after due time.
      */
-    public static String commRecv(ElapsedCpuTimer elapsedTimer, String idStr) {
+    public static String commRecv(ElapsedCpuTimer elapsedTimer, String idStr) throws IOException {
         String ret = null;
 
-        try {
 
-            while(elapsedTimer.remainingTimeMillis() > 0)
-            {
-                if(input.ready()) {
+        while (elapsedTimer.remainingTimeMillis() > 0) {
+            if (input.ready()) {
 
-                    ret = input.readLine();
-                    if (ret != null && ret.trim().length() > 0) {
-                        //System.out.println("TIME OK");
-                        return ret.trim();
-                    }
+                ret = input.readLine();
+                if (ret != null && ret.trim().length() > 0) {
+                    //System.out.println("TIME OK");
+                    return ret.trim();
                 }
             }
         }
-        catch (Exception err) {
-            System.out.println("Exception receiving message: " + err);
-            err.printStackTrace();
-        }
+
 
         //if(elapsedTimer.remainingTimeMillis() <= 0)
         //    System.out.println("TIME OUT (" + idStr + "): " + elapsedTimer.elapsedMillis());
@@ -241,22 +251,18 @@ public class LearningPlayer extends AbstractPlayer{
         return null;
     }
 
-    public final void close()
-    {
-        try
-        {
-            input.close();
-            output.close();
-
-        }catch(IOException e)
-        {
-            System.out.println("IO Exception closing the buffers: " + e);
-            e.printStackTrace();
-        }catch(Exception e)
-        {
-            System.out.println("Exception closing the buffers: " + e);
-            e.printStackTrace();
-        }
-    }
+//    public final void close() {
+//        try {
+//            input.close();
+//            output.close();
+//
+//        } catch (IOException e) {
+//            logger.severe("IO Exception closing the buffers: " + e.getStackTrace());
+//
+//        } catch (Exception e) {
+//            logger.severe("Exception closing the buffers: " + e.getStackTrace());
+//
+//        }
+//    }
 
 }
